c4db228c5e24827ae7b22f5d55f09441e2c4fe31
Hugo Belot-Deloro <hugo.belot-deloro@proton.me>
2025-01-21 13:16:18 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A simple flake template";
+
+  inputs.zig.url = "github:mitchellh/zig-overlay";
+  inputs.zig.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.zls.url = "github:zigtools/zls";
+  inputs.zls.inputs.zig-overlay.follows = "zig";
+  inputs.zls.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, zig, zls, ... }:
+
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      zigpkg = zig.packages.x86_64-linux.master;
+      zlspkg = zls.packages.x86_64-linux.zls;
+      buildInputs = [ zigpkg zlspkg ] ++ (with pkgs; [ qemu_full ]);
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+          inherit buildInputs;
+      };
+
+      formatter.${system} = pkgs.nixfmt;
+    };
+}
