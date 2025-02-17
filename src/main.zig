822c20d47344ac91847f4dd724cc9c3aa8841d31
Hugo Belot-Deloro <hugo.belot-deloro@proton.me>
2025-02-17 22:54:53 +0100

    add serial writer

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,5 @@
+const riscv = @import("riscv.zig");
+
 const bss = @extern([*]u8, .{ .name = "__bss"});
 const bss_end = @extern([*]u8, .{ .name = "__bss_end"});
 const stack_top = @extern([*]u8, .{ .name = "__stack_top"});
@@ -5,10 +7,8 @@ const stack_top = @extern([*]u8, .{ .name = "__stack_top"});
 export fn kernel_main() noreturn {
     @memset(bss[0..bss_end - bss], 0);
 
-    const hello = "\n\nHello World!\n";
-    for (hello) |c| {
-        _ = sbiCall(c, 0, 0, 0, 0, 0, 0, 1);
-    }
+    const hello = "\n\nHello Kernel!\n";
+    _ = try riscv.serialWriter.write(hello);
 
     while (true) asm volatile ("wfi");
 }
@@ -21,38 +21,3 @@ export fn boot() linksection(".text.boot") callconv(.Naked) void {
       : [stack_top] "r" (stack_top)
     );
 }
-
-fn sbiCall(
-    arg0: usize,
-    arg1: usize,
-    arg2: usize,
-    arg3: usize,
-    arg4: usize,
-    arg5: usize,
-    fid: usize,
-    eid: usize
-) SbiRet
-{
-    var err: usize = undefined;
-    var val: usize = undefined;
-
-    asm volatile (
-    "ecall"
-    : [err] "={a0}" (err),
-      [val] "={a1}" (val),
-    : [arg0] "{a0}" (arg0),
-      [arg1] "{a1}" (arg1),
-      [arg2] "{a2}" (arg2),
-      [arg3] "{a3}" (arg3),
-      [arg4] "{a4}" (arg4),
-      [arg5] "{a5}" (arg5),
-      [arg6] "{a6}" (fid),
-      [arg7] "{a7}" (eid),
-    : "memory");
-    return SbiRet { .err = err, .value = val};
-}
-
-pub const SbiRet = struct {
-    err: usize,
-    value: usize,
-};
