cf4206dbf7574d11392178d079a61cf7b2a589fb
Hugo Belot-Deloro <hugo.belot-deloro@proton.me>
2025-02-20 22:24:03 +0100

    alloc

--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,17 @@
 const lib = @import("lib.zig");
 const riscv = @import("riscv.zig");
+const std = @import("std");
 
+const free_ram = @extern([*]u8, .{ .name = "__free_ram" });
+const free_ram_end = @extern([*]u8, .{ .name = "__free_ram_end" });
+
+pub const std_options = std.Options{ .page_size_max = 4096, .page_size_min = 4096 };
+
+const stack_top = @extern([*]u8, .{ .name = "__stack_top" });
 const bss = @extern([*]u8, .{ .name = "__bss" });
 const bss_end = @extern([*]u8, .{ .name = "__bss_end" });
-const stack_top = @extern([*]u8, .{ .name = "__stack_top" });
 
-export fn kernel_main() noreturn {
-    @memset(bss[0 .. bss_end - bss], 0);
-
-    const hello = "\n\nHello Kernel!\n";
-    _ = try lib.serialWriter.write(hello);
-
-    riscv.writeCsr(.stvec, @intFromPtr(&kernel_entry));
-    asm volatile ("unimp");
-
-    lib.panic("test: {d}", .{42}, @src());
-}
-
-export fn boot() linksection(".text.boot") callconv(.Naked) void {
+export fn boot() linksection(".text.boot") callconv(.Naked) noreturn {
     asm volatile (
         \\mv sp, %[stack_top]
         \\j kernel_main
@@ -26,84 +20,39 @@ export fn boot() linksection(".text.boot") callconv(.Naked) void {
     );
 }
 
-export fn kernel_entry() align(4) callconv(.Naked) void {
-    asm volatile (
-        \\csrw sscratch, sp
-        \\addi sp, sp, -4 * 31
-        \\sw ra,  4 * 0(sp)
-        \\sw gp,  4 * 1(sp)
-        \\sw tp,  4 * 2(sp)
-        \\sw t0,  4 * 3(sp)
-        \\sw t1,  4 * 4(sp)
-        \\sw t2,  4 * 5(sp)
-        \\sw t3,  4 * 6(sp)
-        \\sw t4,  4 * 7(sp)
-        \\sw t5,  4 * 8(sp)
-        \\sw t6,  4 * 9(sp)
-        \\sw a0,  4 * 10(sp)
-        \\sw a1,  4 * 11(sp)
-        \\sw a2,  4 * 12(sp)
-        \\sw a3,  4 * 13(sp)
-        \\sw a4,  4 * 14(sp)
-        \\sw a5,  4 * 15(sp)
-        \\sw a6,  4 * 16(sp)
-        \\sw a7,  4 * 17(sp)
-        \\sw s0,  4 * 18(sp)
-        \\sw s1,  4 * 19(sp)
-        \\sw s2,  4 * 20(sp)
-        \\sw s3,  4 * 21(sp)
-        \\sw s4,  4 * 22(sp)
-        \\sw s5,  4 * 23(sp)
-        \\sw s6,  4 * 24(sp)
-        \\sw s7,  4 * 25(sp)
-        \\sw s8,  4 * 26(sp)
-        \\sw s9,  4 * 27(sp)
-        \\sw s10, 4 * 28(sp)
-        \\sw s11, 4 * 29(sp)
-        \\csrr a0, sscratch
-        \\sw a0, 4 * 30(sp)
-        \\mv a0, sp
-        \\call handle_trap
-        \\lw ra,  4 * 0(sp)
-        \\lw gp,  4 * 1(sp)
-        \\lw tp,  4 * 2(sp)
-        \\lw t0,  4 * 3(sp)
-        \\lw t1,  4 * 4(sp)
-        \\lw t2,  4 * 5(sp)
-        \\lw t3,  4 * 6(sp)
-        \\lw t4,  4 * 7(sp)
-        \\lw t5,  4 * 8(sp)
-        \\lw t6,  4 * 9(sp)
-        \\lw a0,  4 * 10(sp)
-        \\lw a1,  4 * 11(sp)
-        \\lw a2,  4 * 12(sp)
-        \\lw a3,  4 * 13(sp)
-        \\lw a4,  4 * 14(sp)
-        \\lw a5,  4 * 15(sp)
-        \\lw a6,  4 * 16(sp)
-        \\lw a7,  4 * 17(sp)
-        \\lw s0,  4 * 18(sp)
-        \\lw s1,  4 * 19(sp)
-        \\lw s2,  4 * 20(sp)
-        \\lw s3,  4 * 21(sp)
-        \\lw s4,  4 * 22(sp)
-        \\lw s5,  4 * 23(sp)
-        \\lw s6,  4 * 24(sp)
-        \\lw s7,  4 * 25(sp)
-        \\lw s8,  4 * 26(sp)
-        \\lw s9,  4 * 27(sp)
-        \\lw s10, 4 * 28(sp)
-        \\lw s11, 4 * 29(sp)
-        \\lw sp,  4 * 30(sp)
-        \\sret
-    );
+export fn kernel_main() noreturn {
+    @memset(bss[0 .. bss_end - bss], 0);
+
+    riscv.setTrapHandler();
+
+    main() catch |err| {
+        lib.serialWriter.print("ERROR: {!}\n", .{err}) catch {};
+    };
+
+    lib.panic("kernel_main end", .{}, @src());
 }
 
-export fn handle_trap(f: *riscv.TrapFrame) void {
-    const scause: usize = riscv.readCsr(.scause);
-    const stval: usize = riscv.readCsr(.stval);
-    const user_pc: usize = riscv.readCsr(.sepc);
+const KAllocator = std.heap.GeneralPurposeAllocator(.{
+    .thread_safe = false,
+});
 
-    lib.panic("unexpected trap scause={x}, stval={x}, sepc={x}\n", .{scause, stval, user_pc}, @src());
-    _ = f;
+// Override the page allocator to avoid Zig trying to import the default page allocator,
+// as it does not exist for freestanding.
+pub const os = struct {
+    pub const heap = struct {
+        pub const page_allocator: std.mem.Allocator = .{
+            .ptr = undefined,
+            .vtable = undefined,
+        };
+    };
+};
+
+pub fn main() !void {
+    var fba = std.heap.FixedBufferAllocator.init(free_ram[0 .. free_ram_end - free_ram]);
+    var allocator = KAllocator{ .backing_allocator = fba.allocator() };
+
+    const a = try allocator.allocator().create(usize);
+    a.* = 42;
+
+    lib.panic("test: {d}, {*}", .{ a.*, a }, @src());
 }
